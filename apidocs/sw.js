d64dbc6db60da6b031db49e67f1796b2dbcf7415
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2019-07-31 00:01:20 +0200

    APIdocs with correct prefix.

--- apidocs/sw.js
+++ apidocs/sw.js
@@ -26,34 +26,34 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d49ff2a09e6410a67c3f.js"
+    "url": "webpack-runtime-557ac3dd86c86b7a98da.js"
   },
   {
-    "url": "styles.22afe6bc875c943006b7.css"
+    "url": "styles.e5e2d9089f9e29b457b0.css"
   },
   {
     "url": "styles-8def39e4520d424c7ed8.js"
   },
   {
-    "url": "app-bc271e68eba5e7a6885e.js"
+    "url": "app-9740f7c069bcefbcb21f.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-3fbffbca62349e6f2f3b.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "cb4346621c699ebac25339097eb93a73"
+    "revision": "587f7d952020a861711cb536d1816ca1"
   },
   {
     "url": "4-6d121881669bb8c95919.js"
   },
   {
     "url": "page-data/404.html/page-data.json",
-    "revision": "100edae0b586a730d31dee8f1fab4e0d"
+    "revision": "cc3f18d445a9c19f159d9e4d0f558a41"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "9928c3edf9f3dd038b626c97c12cfe92"
+    "revision": "0255e2607b280b45b2933c904be0d7ac"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
@@ -75,7 +75,7 @@ const navigationRoute = new workbox.routing.NavigationRoute(({ event }) => {
   return idbKeyval.get(WHITELIST_KEY).then((customWhitelist = []) => {
     // Respond with the offline shell if we match the custom whitelist
     if (customWhitelist.includes(pathname)) {
-      const offlineShell = `/gatsby-starter-apidocs/docs/offline-plugin-app-shell-fallback/index.html`
+      const offlineShell = `/apidocs/offline-plugin-app-shell-fallback/index.html`
       const cacheName = workbox.core.cacheNames.precache
 
       return caches.match(offlineShell, { cacheName }).then(cachedResponse => {
@@ -147,7 +147,7 @@ const messageApi = {
 
     pathnames = pathnames.map(({ pathname, includesPrefix }) => {
       if (!includesPrefix) {
-        return `/gatsby-starter-apidocs/docs${pathname}`
+        return `/apidocs${pathname}`
       } else {
         return pathname
       }
